5346d9b2656f1cc5ee58fed24cb18c23762a71ee
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2023-04-16 19:31:48 +0100

    updated

--- src/middlewares/pre.middleware.ts
+++ src/middlewares/pre.middleware.ts
@@ -1,3 +1,4 @@
+import morgan from 'morgan';
 import cors from "cors";
 import { Application } from "express";
 import * as bodyParser from "body-parser";
@@ -5,6 +6,8 @@ import * as bodyParser from "body-parser";
 export default async (app: Application) => {
   // middleware for parsing application/x-www-form-urlencoded
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use(morgan("dev"))
 
   // middleware for json body parsing
   app.use(bodyParser.json({ limit: "5mb" }));
